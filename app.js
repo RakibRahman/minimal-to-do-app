ffc0956371a359591d6f84506c61a6f306b304ff
RakibRahman <md.rr.talukder@gmail.com>
2021-04-24 02:36:49 +0600

    updated

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const task = document.getElementById("task");
 const addItem = document.getElementById("addItem");
 const output = document.getElementById("output");
-const outputDiv = document.getElementById("showOutput");
-
-// addItem.addEventListener("click", () => {
-//   output.innerHTML = task.value;
-// });
 
 //! add task to list
 const addTask = () => {
@@ -14,6 +9,7 @@ const addTask = () => {
   if (!task.value) return false; //? add no item if its empty
   const item = document.createElement("li");
   item.innerHTML = task.value;
+  item.style.textTransform = "capitalize";
   output.appendChild(item);
   task.value = "";
 };
@@ -23,6 +19,17 @@ addItem.addEventListener("click", addTask);
 // ! remove item from the list
 
 output.addEventListener("click", (event) => {
-  const element = event.target;
-  element.classList.toggle("done");
+  const element = event.target; // select each item of the list
+  if (event.detail === 1) {
+    console.log("crossed");
+    element.classList.toggle("done");
+  } else if (event.detail === 2) {
+    element.style.textDecoration = "none";
+    element.style.color = "black";
+    element.style.content = "🥬";
+    element.contentEditable = true;
+    console.log("edit me now");
+  }
 });
+
+// document.querySelectorAll(".edit-item").contentEditable = true;
